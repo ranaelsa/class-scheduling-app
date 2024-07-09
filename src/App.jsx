8b1e3d05758b3f55354315845c559aa4d673672b
ranaelsa <relsadig@ualberta.ca>
2024-07-09 16:02:03 -0600

    Host on firebase, change app title

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,32 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA7iPc0yr6lF1lR6we78-bHyd2znzOXKtE",
+  authDomain: "class-scheduling-app.firebaseapp.com",
+  projectId: "class-scheduling-app",
+  storageBucket: "class-scheduling-app.appspot.com",
+  messagingSenderId: "450931400523",
+  appId: "1:450931400523:web:5dfb289e7ec71625dd92ce",
+  measurementId: "G-MG3PY7G75T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
 const App = () => {
   const [count, setCount] = useState(0);
 
